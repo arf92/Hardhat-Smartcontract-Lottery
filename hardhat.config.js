052f9c8e43a92011ac8a5b3adf5146acee41ea36
arf92 <arifulfeni92@gmail.com>
2023-03-08 17:37:45 +0600

    v0.2

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
